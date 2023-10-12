6f5d8f18f5eb14725db19c2311a58ba94238b01a
arthurperboni <arthurperboni@hotmail.com>
2023-10-12 04:50:57 -0300

    drop down list

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,7 +1,20 @@
+import Button from "../Button";
+import DropDownList from "../DropDownList";
 import TextSlot from "../TextSlot";
 import "./Formulario.css";
 
 const Formulario = () => {
+
+  const times = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão'
+  ]
+
   return (
     <section className="formulario">
       <form>
@@ -9,6 +22,8 @@ const Formulario = () => {
         <TextSlot label="Nome" placeholder="Digite seu Nome" />
         <TextSlot label="Cargo" placeholder="Digite seu Cargo" />
         <TextSlot label="Imagem" placeholder="Digite o endereço da Imagem" />
+        <DropDownList label="Time" itens={times}/>
+        <Button />
       </form>
     </section>
   );
